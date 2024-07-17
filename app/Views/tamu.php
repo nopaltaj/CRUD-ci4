f74c94c241e2827295df38241779fbcae7c0d903
nopaltaj <tajdhiya.arkan@gmail.com>
2024-07-17 16:19:18 +0700

    ganti url

--- app/Views/tamu.php
+++ app/Views/tamu.php
@@ -7,7 +7,8 @@
     <title>Regis Pegawai</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./datatables/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css">
+    
+    <link rel="stylesheet" href=<?php base_url("https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css")?>>
 
 </head>
 
@@ -39,7 +40,9 @@
         </tbody>
     </table>
     <br>
-    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
+
+
+    <script src=<?php base_url("https://cdn.datatables.net/2.0.8/js/dataTables.min.js")?>></script>
     <script>
         let table = new DataTable('#myTable');
     </script>
@@ -53,7 +56,7 @@
 
 
     <script src="./datatables/datatables.min.js"></script>
-    <script type="text/javascript" src="./datatables/dataTables.bootstrap4.min.js"></script>
+    
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
